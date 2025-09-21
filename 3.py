18c1e26c87cf7fbe804d571d1ddabbf357c79b01
Tammam Bin Masum <tammamxyz@gmail.com>
2025-09-21 19:11:12 +0600

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,7 +1,7 @@
 # bro u have been alive that long yet u only have a 1.4 gpa?
 Name = "tammam"
 Age = 100000000000000000000000000000000000000000000000000000000000000000000000000000
-Gpa = 1.3
+Gpa = 1.4
 Student = true
 
 Bool = bool(Age)
