5cbb4a6c652fba74c94229a90d2aecc8837cde9c
Tammam Bin Masum <tammamxyz@gmail.com>
2025-09-21 19:10:59 +0600

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,4 +1,4 @@
-# bro u have been alive that long yet u only have a 1.3 gpa?
+# bro u have been alive that long yet u only have a 1.4 gpa?
 Name = "tammam"
 Age = 100000000000000000000000000000000000000000000000000000000000000000000000000000
 Gpa = 1.3
